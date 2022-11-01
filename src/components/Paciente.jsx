b206e84ca106ec950220fa4c340a15c435eecf34
franqg99 <franqg99@gmail.com>
2022-11-01 13:10:54 +0000

    This commit will finish styling components

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,13 @@
+function Pacientes() {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">Mombre: <span className="font-normal normal-case">Sambi</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: <span className="font-normal normal-case">Francisco Quintana</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Email: <span className="font-normal normal-case">fquintana@protonmail.com</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Alta: <span className="font-normal normal-case">08 Noviembre de 2022</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</span></p>
+    </div>
+  )
+}
+
+export default Pacientes
